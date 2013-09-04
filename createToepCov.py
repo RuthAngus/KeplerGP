b74ee32d044bf1255b47bb1c842930bbb1da3786
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2013-09-04 17:14:35 -0400

    with delta

--- createToepCov.py
+++ createToepCov.py
@@ -16,30 +16,25 @@ def createToepCov(M,T,C,Y,sv):
 # C array which contain all the time vector C=vstack((C0,C1,C2 ...))
 # sv vector which contain the lenght of vector C and T, N[0]=size(T0),size(C0)...
     global n
-    n = 0 # mean value of the time gap
-    d = np.zeros((M,1))
+    n=0 # mean value of the time gap
+    d=np.zeros((M,1))
     for i in range(0,M):
-        Te = T[n:n+sv[i],:] 
-        Ce = C[n:n+sv[i],:]
-        Ye = Y[n:n+sv[i],:]
-        # Remove nanas
-        T_nan = np.isnan(Te)
-        Y_nan = np.isnan(Ye)
+        Te=T[n:n+sv[i],:]
+        Ce=C[n:n+sv[i],:]
+        Ye=Y[n:n+sv[i],:]
+        T_nan=np.isnan(Te)
+        Y_nan=np.isnan(Ye)
         ind1,j=np.where(T_nan==False)
         ind2,j=np.where(Y_nan==False)
-        # ind1 and ind2 are indices of nans
         if i==0 :
-            # For the first array (q2), Cf is the q2 cadence
             Cf=Ce
             Yf=Ye
             N=np.shape(Cf)[0]
             inds1=ind1
             inds2=ind2
         else:
-            # k is the first cadence in q2 minus the last in the last cadence array - 1
-            k = Ce[0] - Cf[-1] - 1
-            Cf = np.vstack( (Cf, np.vstack(np.r_[ (Cf[-1] + 1):(Ce[0]) ])) )
-
+            k=Ce[0]-Cf[-1]-1
+            Cf=np.vstack((Cf,np.vstack(np.r_[(Cf[-1]+1):(Ce[0])])))
             N=np.shape(Cf)[0]
             inds1=np.hstack((inds1,N+ind1))
             
@@ -58,10 +53,9 @@ def createToepCov(M,T,C,Y,sv):
     # Intersect inds1 and inds2
     inds=np.intersect1d(inds1,inds2)
 
-    # return Cf,inds,delta,Yf
-    return Cf, inds, Yf
+    return Cf,inds,delta,Yf
 
-def createMatCov(M, T, C, Y, sv, f):
+def createMatCov(M,T,C,Y,sv,f):
 # c,r first column and first row of the covariance matrix
 # inds vector who contain the indice we need for make the multiplication...
     
@@ -71,17 +65,11 @@ def createMatCov(M, T, C, Y, sv, f):
 # sv vector which contain the lenght of vector C and T, N[0]=size(T0),size(C0)...
 # f kernel !!f should work with vector!!
 
-    # Cf, inds, delta, Yf = createToepCov(M, T, C, Y, sv)
-    Cf, inds, Yf = createToepCov(M, T, C, Y, sv)
+    Cf,inds,delta,Yf=createToepCov(M,T,C,Y,sv)
     
-    # c = f(Cf, Cf[0], delta)
-    # print Cf
-    # print Cf[0]
-    # print len(Cf), type(Cf), np.shape(Cf)
-    # print len(Cf[0]), type(Cf[0]), np.shape(Cf[0])
-    c = f(Cf, Cf[0])
-    r = c.T
-    return c, r, inds, Yf
+    c=f(Cf,Cf[0],delta)
+    r=c.T
+    return c,r,inds,Yf
     
 # for make a multiplication K*x used pdtMatToepVect(c,r,x,inds) 
     # x should have the size of inds

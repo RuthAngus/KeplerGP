8a9d48d95dbf3a7e82239fd13568f4f16a216ad0
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2014-05-22 10:28:08 +0100

    adding stuff

--- rotation/george/ml_grid.py
+++ rotation/george/ml_grid.py
@@ -10,7 +10,7 @@ import scipy.optimize as so
 import time
 from matplotlib.ticker import MaxNLocator
 from scipy.optimize import fmin
-from scipy.signal import periodogram, find_peaks_cwt
+from scipy.signal import periodogram, find_peaks_cwt, argrelextrema
 import scipy as sp
 import pylab as pb
 
@@ -65,7 +65,7 @@ def MCMC(theta, x, y, yerr, bm, bp):
     nwalkers, ndim = 64, len(theta)
     p0 = [theta+1e-4*np.random.rand(ndim) for i in range(nwalkers)]
     sampler = emcee.EnsembleSampler(nwalkers, ndim, lnprob, args = (x, y, yerr, bm, bp))
-    bi, pr = 200, 800
+    bi, pr = 200, 2000
     start = time.clock()
     print("Burn-in")
     p0, lp, state = sampler.run_mcmc(p0, bi)
@@ -109,7 +109,16 @@ def MCMC(theta, x, y, yerr, bm, bp):
     pl.xlabel('time (days)')
     pl.savefig('result')
 
-def global_max(x, y, yerr, theta, P, r, s, b):
+def find_range(P, r, s):
+    # Grid over periods
+    if type(r)==tuple:
+        mn, mx = r
+    else:
+        mn, mx = P-(P*r), P+(P*r)
+    step = (mx-mn)/s
+    return np.arange(mn, mx, step)
+
+def global_max(x, y, yerr, theta, Periods, P, r, s, b, save):
 
         print "Initial parameters = (exp)", theta
         start = time.clock()
@@ -117,20 +126,15 @@ def global_max(x, y, yerr, theta, P, r, s, b):
         elapsed = (time.clock() - start)
         print 'time =', elapsed
 
-        # Grid over periods
-        mn, mx = P-(P*r), P+(P*r)
-        step = (mx-mn)/s
-        Periods = np.arange(mn, mx, step)
         L = np.zeros_like(Periods)
         results = np.zeros((len(L), 4))
 
         for i, p in enumerate(Periods):
             L[i], results[i,:] = maxlike(theta, x, y, yerr, p, i)
 
-        np.savetxt('%sml_results.txt' %int(KID), np.transpose((Periods, L)))
-
-        mlp = Periods[L == max(L)]
-        mlresult = results[L == max(L)]
+        iL = L==max(L)
+        mlp = Periods[iL]
+        mlresult = results[iL]
         print 'max likelihood period = ', mlp
 
         print 'mlresult = ', mlresult[0]
@@ -143,7 +147,7 @@ def global_max(x, y, yerr, theta, P, r, s, b):
         pl.xlabel('$\mathrm{Time~(days)}$')
         pl.ylabel('$\mathrm{Normalised~Flux}$')
         pl.gca().yaxis.set_major_locator(MaxNLocator(prune='lower'))
-        pl.savefig('%sml_data' %int(KID))
+        pl.savefig('%sml_data%s' %(int(KID), save))
 
         # plot GPeriodogram
         pl.clf()
@@ -152,15 +156,19 @@ def global_max(x, y, yerr, theta, P, r, s, b):
         pl.xlabel('Period')
         pl.ylabel('Likelihood')
         pl.title('Period = %s' %mlp)
-        pl.savefig('%sml_likelihood' %int(KID))
+        pl.savefig('%sml_likelihood%s' %(int(KID), save))
 
         # set period prior boundaries
         bm = mlp - b*mlp
         bp = mlp + b*mlp
 
-        return mlp, bm, bp
+        return L, mlp, bm, bp, mlresult[0]
 
 def autocorrelation(x, y):
+
+    # normalise so range is 2 - no idea if this is the right thing to do...
+    y = 2*y/(max(y)-min(y))
+    y = y-np.median(y)
 
     # calculate autocorrelation fn
     lags, acf, lines, axis = pb.acorr(y, maxlags = len(y)/2.)
@@ -221,6 +229,33 @@ def pgram(y, r, P, highres):
         "ValueError:"
         pass
 
+# subsample and truncate data
+def subs(x, y, yerr, p_init, l):
+    nday = 48. # number of data points/day
+    npoints = 100. # number of data points needed/period
+    subsamp = int(round(nday*p_init/npoints))
+    if subsamp > 0:
+        x = x[::subsamp]
+        y = y[::subsamp]
+        yerr = yerr[::subsamp]
+
+    return x[:l], y[:l], yerr[:l]
+
+def find_mins(L):
+    # recalculate period range
+    iL = np.where(L==max(L))[0]
+    try:
+        lmin = max(argrelextrema(L[:iL+1], np.less)[0])
+    except:
+        "max() arg is an empty sequence"
+        lmin = 0
+    try:
+        rmin = int(min(argrelextrema(L[iL-1:], np.less)[0])+(iL-1))
+    except:
+        "min() arg is an empty sequence"
+        rmin = -1
+    return lmin, rmin
+
 if __name__ == "__main__":
 
     cadence = 0.02043365
@@ -228,61 +263,77 @@ if __name__ == "__main__":
     # Load target list with ACF periods
     data = np.genfromtxt('/Users/angusr/Python/george/targets.txt').T
     KIDs = data[0]
-    p_init = data[1]
+#     p_init = data[1]
 
-#     KIDs = KIDs[1:]
-#     p_init = p_init[1:]
+    save_results = np.zeros((len(KIDs), 8))
 
     for k, KID in enumerate(KIDs):
 
+#         p_init = p_init[k]
         print k, KID
 
         # Load real quarter 3 data
         x, y, yerr = load("/Users/angusr/angusr/data2/Q3_public/kplr0%s-2009350155506_llc.fits" %int(KID))
 
         r = .4 # range of periods to try
-        s = 10. # number of periods to try
+        s = 30. # number of periods to try
         b = .2 # prior boundaries
 
-        pgram(y, r, p_init[k], highres = True)
+        # compute acf
+        p_init = autocorrelation(x, y)
+
+        # compute lomb scargle periodogram
+        pgram(y, r, p_init, highres = True)
 
         # normalise so range is 2 - no idea if this is the right thing to do...
         yerr = 2*yerr/(max(y)-min(y))
         y = 2*y/(max(y)-min(y))
         y = y-np.median(y)
 
-        autocorrelation(x, y)
+        # subsample and truncate
+        x_sub, y_sub, yerr_sub = subs(x, y, yerr, p_init, 500.)
 
-        # subsample and truncate data
-        nday = 48. # number of data points/day
-        npoints = 100. # number of data points needed/period
-        subsamp = int(round(nday*p_init[k]/npoints))
-        if subsamp > 0:
-            x = x[::subsamp]
-            y = y[::subsamp]
-            yerr = yerr[::subsamp]
-
-        l = 500. # truncate to 500 data points, total
-        x = x[:l]
-        y = y[:l]
-        yerr = yerr[:l]
-
-#         theta, P = [-2., -2., -1.2, 1.], 1.7 # generating fake data
         theta = [-2., -2., -1.2, 1.]
 
 #         # generate fake data
 #         K = QP(theta, x, yerr, P)
 #         y = np.random.multivariate_normal(np.zeros(len(x)), K)
 
-#         raw_input('enter')
+        # find range of periods to calculate L over
+        Periods = find_range(p_init, r, s)
 
-        # THIS NEEDS TO RETURN ALL MAX LIKELIHOOD PARAMS
-        mlp, bm, bp = global_max(x, y, yerr, theta, p_init[k], r, s, b)
+        # Find first global max
+        L, mlp, bm, bp, mlh = global_max(x_sub, y_sub, yerr_sub, theta, Periods, p_init, \
+                r, s, b, '1')
+
+        np.savetxt('%sml_results.txt' %int(KID), np.transpose((Periods, L)))
+
+        # find minima either side of peak
+        #FIXME: maybe I should have a bit of leeway either side of the peak?
+        lmin, rmin = find_mins(L)
+        r = (Periods[lmin], Periods[rmin])
+        Periods = find_range(mlp, r, s)
+
+        # zoom in on highest peak
+        L, mlp, bm, bp, mlh = global_max(x_sub, y_sub, yerr_sub, theta, Periods, p_init, \
+                r, s, b, '2')
+
+        save_results[k,:] = np.array([KID, mlp, r[0], r[1], mlh[0], mlh[1], mlh[2], mlh[3]])
+        print 'saving'
+        print save_results
+        np.savetxt('results.txt', save_results)
 
 #         # running MCMC over maxlikelihood period
+#         data = np.genfromtxt('/Users/angusr/Python/george/results.txt').T
+#         r0 = data[2][0]
+#         r1 = data[3][0]
+#         m = [data[4][0], data[5][0], data[6][0], data[7][0], data[1][0]]
+#         print m
+#         raw_input('enter')
+#         MCMC(m, x, y, yerr, r0, r1)
+#         raw_input('enter')
+
 #         m = np.empty(len(theta)+1)
 #         m[:len(theta)] = theta
 #         m[-1] = mlp
-# #         m = [-2.16545599, -0.12804739, -0.19512039, 3.4567929, 2.496]
-# #         bm, bp = 2., 3.
-#         MCMC(m, x, y, yerr, bm, bp)
+#         MCMC(m, x, y, yerr, r[0], r[1])
